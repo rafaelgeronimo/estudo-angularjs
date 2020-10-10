96c640dbe9706381ea8f01b72921289bf3e7e3ad
Rafael Gerônimo <rafaelg@VEGA.INTRA>
2020-10-10 11:07:15 -0300

    usando diretivas parte 2

--- js/lista.js
+++ js/lista.js
@@ -6,8 +6,25 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($s
     {nome: "Ana", telefone: "99998877"},
     {nome: "Maria", telefone: "99998866"},
   ];
+  $scope.operadoras = [
+    {nome: "Oi", codigo: 14, categoria: "Celular"},
+    {nome: "Vivo", codigo: 15, categoria: "Celular"},
+    {nome: "Tim", codigo: 41, categoria: "Celular"},
+    {nome: "GVT", codigo: 25, categoria: "Fixo"},
+    {nome: "Embratel", codigo: 21, categoria: "Fixo"}
+  ];
   $scope.adicionarContato = function(contato) {
     $scope.contatos.push(angular.copy(contato));
     delete $scope.contato;
+  };
+  $scope.apagarContatos = function(contatos) {
+    $scope.contatos = contatos.filter(function(contato) {
+        if (!contato.selecionado) return contato;
+    });
+  };
+  $scope.isContatoSelecionado = function(contatos) {
+    return contatos.some(function (contato) {
+      return contato.selecionado;
+    });
   };
 });
